32661b70a3d8fe8508cc7768434fcea3ace4e64d
FrankieTin <1737838404@qq.com>
2026-04-23 05:09:14 +0800

    顶部栏调整

--- src/App.tsx
+++ src/App.tsx
@@ -87,7 +87,7 @@ function AppContent() {
                 </button>
               </div>
 
-              <div className="max-w-6xl mx-auto p-4 sm:p-6 md:p-10 md:pt-16 space-y-6">
+              <div className="max-w-6xl mx-auto px-4 pt-2 pb-4 sm:p-6 md:p-10 md:pt-16 space-y-6">
                 {currentTab === 'time' && <TimeRecord />}
                 {currentTab === 'plan' && <ResearchPlan />}
                 {currentTab === 'fitness' && <FitnessRecord />}
